fd3fffdfe16c1d9aa293c7cdc66375d29de64609
Xagar123 <sagarcoc2000@gmail.com>
2022-09-12 14:58:37 +0530

    Stock account program

---
+++ src/StockAccount.java
@@ -0,0 +1,38 @@
+public class StockAccount {
+    private String stockName;
+    private int noOfShares;
+    private double sharePrice;
+    private double shareTotalValue;
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getNoOfShares() {
+        return noOfShares;
+    }
+
+    public void setNoOfShares(int noOfShares) {
+        this.noOfShares = noOfShares;
+    }
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(double sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    public double getShareTotalValue() {
+        return shareTotalValue;
+    }
+
+    public void setShareTotalValue(double shareTotalValue, double sharePrice) {
+        this.shareTotalValue = shareTotalValue;
+    }
+}
